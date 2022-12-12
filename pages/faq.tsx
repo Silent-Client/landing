d349f63b0e913af5c993a30a90564181bb098068
kirillsaint <kirillsaint@wtfuck.tk>
2022-12-12 10:16:20 +0600

    Better questions

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -14,7 +14,97 @@ import {
 import React from "react";
 import Head from "next/head";
 
+export const questions: {
+	question: string;
+	answer: React.ReactNode;
+}[] = [
+	{
+		question: "What is Silent Client?",
+		answer: (
+			<p>
+				Silent Client is a Minecraft client with built-in FPS boost, mods and
+				more.
+				<br /> We are actively working to improve the client and add innovations
+				that our players have asked us for.
+			</p>
+		),
+	},
+	{
+		question: "Can I use Silent Client on any server?",
+		answer: (
+			<p>
+				Silent Client supports all servers supported by Minecraft version 1.8.9.
+			</p>
+		),
+	},
+	{
+		question: "Is Silent Client really free??",
+		answer: (
+			<p>
+				Silent Client is completely free to use, and you miss out on zero
+				features! However, if you would like you can support us by purchasing
+				cosmetics in-game to improve your look, show off to your friends and
+				improve the way your character looks at{" "}
+				<Link
+					textDecoration="underline"
+					color="white"
+					href={"https://store.silentclient.ml"}
+				>
+					store.silentclient.ml
+				</Link>
+				.
+			</p>
+		),
+	},
+];
+
 function Faq() {
+	const Item = ({
+		question,
+		answer,
+	}: {
+		question: string;
+		answer: React.ReactNode;
+	}) => (
+		<AccordionItem mb={5} borderTop="none" borderBottom="none">
+			<AccordionButton
+				borderTop="none"
+				borderBottom="none"
+				paddingTop={"2rem"}
+				paddingBottom={"2rem"}
+				paddingRight="1.5rem"
+				paddingLeft="1.5rem"
+				borderRadius="1.15rem"
+				bgColor="rgb(19, 19, 19)"
+				h="full"
+				display="block"
+				textAlign="left"
+				_hover={{}}
+			>
+				<Center h="full" w="full">
+					<Stack w="full" direction="row" justifyContent="space-between">
+						<Heading textAlign="left" size="sm">
+							{question}
+						</Heading>
+						<AccordionIcon />
+					</Stack>
+				</Center>
+				<AccordionPanel
+					color="#c9c9c9"
+					fontSize="md"
+					lineHeight="1.5"
+					mt={5}
+					mb={0}
+					pb={0}
+					pr={0}
+					pl={0}
+				>
+					{answer}
+				</AccordionPanel>
+			</AccordionButton>
+		</AccordionItem>
+	);
+
 	return (
 		<Box w="full">
 			<Head>
@@ -34,128 +124,9 @@ function Faq() {
 			</Stack>
 			<Stack direction="column" spacing={5} mt={20}>
 				<Accordion allowToggle={true}>
-					<AccordionItem
-						mb={5}
-						borderTop="none"
-						borderBottom="none"
-						paddingTop={"2rem"}
-						paddingBottom={"2rem"}
-						paddingRight="1.5rem"
-						paddingLeft="1.5rem"
-						borderRadius="1.15rem"
-						bgColor="rgb(19, 19, 19)"
-					>
-						<AccordionButton h="full" padding={0}>
-							<Center h="full" w="full">
-								<Stack w="full" direction="row" justifyContent="space-between">
-									<Heading textAlign="left" size="sm">
-										What is Silent Client?
-									</Heading>
-									<AccordionIcon />
-								</Stack>
-							</Center>
-						</AccordionButton>
-
-						<AccordionPanel
-							color="#c9c9c9"
-							fontSize="md"
-							lineHeight="1.5"
-							mt={5}
-							mb={0}
-							pb={0}
-							pr={0}
-							pl={0}
-						>
-							Silent Client is a Minecraft client with built-in FPS boost, mods
-							and more.
-							<br /> We are actively working to improve the client and add
-							innovations that our players have asked us for.
-						</AccordionPanel>
-					</AccordionItem>
-
-					<AccordionItem
-						mb={5}
-						borderTop="none"
-						borderBottom="none"
-						paddingTop={"2rem"}
-						paddingBottom={"2rem"}
-						paddingRight="1.5rem"
-						paddingLeft="1.5rem"
-						borderRadius="1.15rem"
-						bgColor="rgb(19, 19, 19)"
-					>
-						<AccordionButton h="full" padding={0}>
-							<Center h="full" w="full">
-								<Stack w="full" direction="row" justifyContent="space-between">
-									<Heading textAlign="left" size="sm">
-										Can I use Silent Client on any server?
-									</Heading>
-									<AccordionIcon />
-								</Stack>
-							</Center>
-						</AccordionButton>
-
-						<AccordionPanel
-							color="#c9c9c9"
-							fontSize="md"
-							lineHeight="1.5"
-							mt={5}
-							mb={0}
-							pb={0}
-							pr={0}
-							pl={0}
-						>
-							Silent Client supports all servers supported by Minecraft version
-							1.8.9.
-						</AccordionPanel>
-					</AccordionItem>
-
-					<AccordionItem
-						mb={5}
-						borderTop="none"
-						borderBottom="none"
-						paddingTop={"2rem"}
-						paddingBottom={"2rem"}
-						paddingRight="1.5rem"
-						paddingLeft="1.5rem"
-						borderRadius="1.15rem"
-						bgColor="rgb(19, 19, 19)"
-					>
-						<AccordionButton h="full" padding={0}>
-							<Center h="full" w="full">
-								<Stack w="full" direction="row" justifyContent="space-between">
-									<Heading textAlign="left" size="sm">
-										Is Silent Client really free?
-									</Heading>
-									<AccordionIcon />
-								</Stack>
-							</Center>
-						</AccordionButton>
-
-						<AccordionPanel
-							color="#c9c9c9"
-							fontSize="md"
-							lineHeight="1.5"
-							mt={5}
-							mb={0}
-							pb={0}
-							pr={0}
-							pl={0}
-						>
-							Silent Client is completely free to use, and you miss out on zero
-							features! However, if you would like you can support us by
-							purchasing cosmetics in-game to improve your look, show off to
-							your friends and improve the way your character looks at{" "}
-							<Link
-								textDecoration="underline"
-								color="white"
-								href={"https://store.silentclient.ml"}
-							>
-								store.silentclient.ml
-							</Link>
-							.
-						</AccordionPanel>
-					</AccordionItem>
+					{questions.map(q => (
+						<Item question={q.question} answer={q.answer} />
+					))}
 				</Accordion>
 			</Stack>
 		</Box>
